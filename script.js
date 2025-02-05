1d5f301c6d1c72af171a3f5b733877b9fd8bb091
Raviteja Shivanadhuni <ravitejashivanadhuni@gmail.com>
2025-02-05 16:10:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", event => {
+            event.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
