b7f1e24dac56ccbd0a4a5d496af99ff08e93a1b6
Raviteja Shivanadhuni <ravitejashivanadhuni@gmail.com>
2025-02-07 13:28:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function redirectTo(projectPage) {
+    window.location.href = projectPage;
+}
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();  // Prevent the form from submitting traditionally
 
